fa0f8230e0e895e3d2905a2e9e6849c5dfc9d486
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2023-01-09 12:53:57 -0800

    chore: Add support for 'sh' in 'libyaml.sh'

--- packaging/build/libyaml.sh
+++ packaging/build/libyaml.sh
@@ -1,26 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
 set -eux
 
-# build the requested version of libyaml locally
+# Build the requested version of libyaml locally
 echo "::group::fetch libyaml ${LIBYAML_REF}"
 git config --global advice.detachedHead false
 git clone --branch "$LIBYAML_REF" "$LIBYAML_REPO" libyaml
-pushd libyaml
-git reset --hard "$LIBYAML_REF"
-echo "::endgroup::"
 
-echo "::group::autoconf libyaml w/ static only"
-./bootstrap
-# build only a static library- reduces our reliance on auditwheel/delocate magic
-./configure --disable-dependency-tracking --with-pic --enable-shared=no
-echo "::endgroup::"
+# Use a subshell to avoid polluting the current directory
+(
+    set -eux
+    cd libyaml || true
+    git reset --hard "$LIBYAML_REF"
+    echo "::endgroup::"
 
-echo "::group::build libyaml"
-make
-echo "::endgroup::"
+    # Build only a static library - this reduces our reliance on auditwheel/delocate magic
+    echo "::group::autoconf libyaml w/ static only"
+    ./bootstrap
+    ./configure --disable-dependency-tracking --with-pic --enable-shared=no
+    echo "::endgroup::"
 
-echo "::group::test built libyaml"
-make test-all
-echo "::endgroup::"
-popd
+    echo "::group::build libyaml"
+    make
+    echo "::endgroup::"
+
+    echo "::group::test built libyaml"
+    make test-all
+    echo "::endgroup::"
+)
