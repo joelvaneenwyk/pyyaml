1a1282062cb6b4c724703edd4a7400be53207a66
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2023-01-11 11:03:21 -0800

    Add Windows setup script for installing Python versions

---
+++ setup.bat
@@ -0,0 +1,37 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+if "%~1"=="clean" (
+    rmdir /q /s "%~dp0.tox"
+    rmdir /q /s "%~dp0libyaml"
+    rmdir /q /s "%~dp0build"
+    rmdir /q /s "%~dp0lib\PyYAML.egg-info"
+)
+
+if "%~1"=="install" (
+    echo Installing Python versions through 'pyenv' install...
+    call pyenv install 2.6.6
+    call pyenv install 2.7.18
+    call pyenv install 3.10.9
+    call pyenv install 3.11.1
+    call pyenv install 3.12.0a3
+    call pyenv install 3.6.8
+    call pyenv install 3.7.0-win32
+    call pyenv install 3.8.10
+    call pyenv install 3.9.9
+
+    echo Installing Python versions through 'scoop' install...
+    scoop bucket add versions
+    scoop install python27
+    scoop install python36
+    scoop install python37
+    scoop install python311
+    scoop install pypy2
+    scoop install pypy3
+)
+
+call pyenv exec python --version
+call pyenv exec python -m ensurepip
+call pyenv exec python -m pip install --upgrade pip
+call pyenv exec python -m pip install tox
+call pyenv exec python -m tox run-parallel
